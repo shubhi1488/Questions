fe2c70bd6d4e3dd79467ca6e95c343cc927ba6d4
HP <shubhi1488@gmail.com>
2021-09-27 23:49:20 +0530

    Initial commit

---
+++ src/Pallindrome.java
@@ -0,0 +1,18 @@
+public class Pallindrome {
+    public static void main(String[] args){
+        int r,sum=0,temp;
+        int n=123;
+        temp=n;
+        while (n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+            System.out.println("pallindrome number");
+        else
+            System.out.println("not a pallindrome number");
+
+
+    }
+}
